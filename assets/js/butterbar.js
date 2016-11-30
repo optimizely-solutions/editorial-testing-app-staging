b8f9f5f701eb76c92a86467a7bb6cc1b1f2d6596
trichter <thilo@frogdot.de>
2016-11-30 13:46:41 +0100

    log definition

--- assets/js/butterbar.js
+++ assets/js/butterbar.js
@@ -1,5 +1,8 @@
-var close = document.getElementById("close");
-close.addEventListener('click', function() {
- note = document.getElementById("note");
- note.style.display = 'none';
-}, false);
+$(document).ready(function($) {
+  var close = document.getElementById("close");
+  close.addEventListener('click', function() {
+   note = document.getElementById("note");
+   note.style.display = 'none';
+  }, false);
+});
+
